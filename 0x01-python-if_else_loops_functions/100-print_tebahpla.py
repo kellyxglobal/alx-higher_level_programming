7ec6ea9032ba7ec286fd82c46e6901f3e80e18ab
kellyxglobal <Kellyxglobal@gmail.com>
2022-08-23 14:38:57 -0700

    100th commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    alt = True
-    if i % 2 != 0:
-        alt = False
-    print("{}".format(chr(i) if alt else chr(i - 32)), end='')
+for i in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(i, chr(i - 33)), end="")
