7b897c34e54e52980489a61f10d8bf48649e254e
kellyxglobal <Kellyxglobal@gmail.com>
2022-08-25 13:12:28 -0700

    5th commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
