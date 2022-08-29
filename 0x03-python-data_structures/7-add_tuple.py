f170c5f854b2fe76587967c0af365c453c58303f
kellyxglobal <Kellyxglobal@gmail.com>
2022-08-29 14:50:14 -0700

    a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len1 = len(tuple_a)
-    len2 = len(tuple_b)
-
-    if (len1 >= 2 and len2 >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif (len1 == 1 and len2 == 1):
-        return (tuple_a[0] + tuple_b[0], 0)
-    elif (len1 == 0 and len2 == 0):
-        return (0, 0)
-    elif (len1 == 1 and len2 >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_b[1])
-    elif (len1 == 0 and len2 >= 2):
-        return (tuple_b[0], tuple_b[1])
-    elif (len1 >= 2 and len2 == 1):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif (len1 >= 2 and len2 == 0):
-        return (tuple_a[0], tuple_a[1])
-    elif (len1 == 1 and len2 == 0):
-        return (tuple_a[0], 0)
-    elif (len1 == 0 and len2 == 1):
-        return (tuple_b[0], 0)
+    sum1 = 0
+    sum2 = 0
+    if (len(tuple_a) == 0):
+        a1 = 0
+        a2 = 0
+    elif(len(tuple_a) == 1):
+        a1 = tuple_a[0]
+        a2 = 0
+    else:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    if (len(tuple_b) == 0):
+        b1 = 0
+        b2 = 0
+    elif(len(tuple_b) == 1):
+        b1 = tuple_b[0]
+        b2 = 0
+    else:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+    new = (sum1, sum2)
+    return (new)
