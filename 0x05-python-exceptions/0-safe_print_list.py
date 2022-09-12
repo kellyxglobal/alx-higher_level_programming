cfbbeaf23ba513b0788c106d089ab38b872d809e
kellyxglobal <Kellyxglobal@gmail.com>
2022-09-12 16:05:18 -0700

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        while i is not x:
-            print(my_list[i], end="")
-            i += 1
-    except IndexError:
-        None
-    print()
-    return
+    count = 0
 
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except (IndexError, TypeError):
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
