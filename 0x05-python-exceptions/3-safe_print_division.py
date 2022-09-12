15a5971c469687f7817546a258ca3b5356beb732
kellyxglobal <Kellyxglobal@gmail.com>
2022-09-12 16:21:52 -0700

    commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        q = a / b
+        result = a / b
     except ZeroDivisionError:
-        q = None
+        result = None
     finally:
-        print("Inside result: {}".format(q))
-    return 
+        print("Inside result: {}".format(result))
+        return (result) 
