d23cf9e6618b815994f5960168fd379bcb3d8805
Kellyxglobal-WebS <39860700+kellyxglobal@users.noreply.github.com>
2022-09-27 21:25:21 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" Module 7-add_item
-that adds all arguments to a Python list, and then
-save them to a file
-"""
+"""Module for a script that adds all arguments to a Python list"""
+import json
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-import sys
-import os.path
 
-save_file = __import__('5-save_to_json_file').save_to_json_file
-load_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+# can also use os.path.exists(file) for try and except
+# be sure to import os.path
+try:
+    json_list = load_from_json_file(file_name)
+except:
+    json_list = []
+for item in argv[1:]:
+    json_list.append(item)
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_file(my_list, "add_item.json")
+save_to_json_file(json_list, file_name)
