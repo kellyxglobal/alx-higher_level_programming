51d8a5cd36ef517489927ec2b345bace260ec1f4
kellyxglobal <Kellyxglobal@gmail.com>
2022-11-29 08:50:29 -0800

    commit

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-module.exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
+// Executes theFunction 'x' times
+exports.callMeMoby = function (x, theFunction) {
+  while (x-- > 0) {
     theFunction();
   }
 };
